640bdacfcc781a40d2efd801d7b3e13ec7671c3f
Alexei Cazacu <alexvspishka@gmail.com>
2022-11-22 12:23:34 +0100

    starting controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,24 @@
 class OffersController < ApplicationController
+  def index
+    @offers = Offer.all
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
