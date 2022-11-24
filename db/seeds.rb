dc880f94e792507e15951a166d402aaab5d4b69f
sophie Parisot <sophie.parisot.93@gmail.com>
2022-11-24 17:07:26 +0100

    updatedprofilwithwallet and counts

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,15 @@ Session.destroy_all
 Offer.destroy_all
 User.destroy_all
 puts 'creating users'
-User.create(email: 'alex@mail.com', password: 'azerty', username: 'Alex', bio: 'Coach de foot', experience: 3);
-User.create(email: 'farnci@mail.com', password: 'azerty', username: 'Franci', bio: 'Coach de running', experience: 5);
-User.create(email: 'sophie@mail.com', password: 'azerty', username: 'Sophie');
-User.create(email: 'vannesa@mail.com', password: 'azerty', username: 'Vannesa');
+User.create!(email: 'alex@mail.com', password: 'azerty', username: 'Alex', bio: 'Coach de foot', experience: 3);
+User.create!(email: 'farnci@mail.com', password: 'azerty', username: 'Franci', bio: 'Coach de running', experience: 5);
+User.create!(email: 'sophie@mail.com', password: 'azerty', username: 'Sophie', bio: 'Coache d"escalade', experience: 5);
+User.create!(email: 'vannesa@mail.com', password: 'azerty', username: 'Vannesa');
 
 User.all.each do  |user|
-  Offer.create(name: 'Escalade pour les debutants', sport: 'Escalade', price: 25, user: user)
-  Offer.create(name: 'Escalade pour les experimentes', sport: 'Escalade', price: 55, user: user)
-  Offer.create(name: 'Running pour les debutants', sport: 'Running', price: 25, user: user)
-  Offer.create(name: 'Tennis pour les debutants', sport: 'Tennis', price: 35, user: user)
+  Offer.create!(name: 'Escalade pour les debutants', sport: 'Escalade', price: 25, user: user, niveau:5)
+  Offer.create!(name: 'Escalade pour les experimentes', sport: 'Escalade', price: 55, user: user, niveau:3)
+  Offer.create!(name: 'Running pour les debutants', sport: 'Running', price: 25, user: user, niveau:2)
+  Offer.create!(name: 'Tennis pour les debutants', sport: 'Tennis', price: 35, user: user, niveau:1)
 end
 puts 'all id good !'
