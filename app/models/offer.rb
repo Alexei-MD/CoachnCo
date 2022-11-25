1a8a07ca63974308d0561161b7ad3cc30c1cd014
Alexei Cazacu <alexvspishka@gmail.com>
2022-11-25 11:39:57 +0100

    has-many pour le offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,12 +1,13 @@
 class Offer < ApplicationRecord
   belongs_to :user
+  has_many :sessions, dependent: :destroy
   has_one_attached :photo
   SPORT = %w(Escalade Tennis Swimming Running Golf Gym)
   NIV = %w(1 2 3 4 5)
-  
+
   validates :name, presence: true
   validates :sport, presence: true
   validates :price, presence: true
   validates :niveau, presence: true
-  
+
 end
