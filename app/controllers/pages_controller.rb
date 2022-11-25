d04c79e245a1547293bd5df3922d5d399b4c69ba
sophie Parisot <sophie.parisot.93@gmail.com>
2022-11-25 13:10:23 +0100

    walletupdate

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,17 @@ class PagesController < ApplicationController
   end
 
   def profil
-  @user = current_user
+    @user = current_user
+    @sum = 0
+    @offers = Offer.where(user_id: @user)
+    if @offers.count > 0
+      @offers.each do |offer|
+        @sessions = Session.where(offer_id: offer.id)
+        if @sessions.length > 0
+          @sum += offer.price * @sessions.length
+        end
+        @sum
+      end
+    end
   end
 end
